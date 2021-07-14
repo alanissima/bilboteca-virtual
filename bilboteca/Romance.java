d77ac03b2949f8a30497dcf76ce5fbf020aa23f2
nicollytavassi <55161442+nicollytavassi@users.noreply.github.com>
2021-07-14 04:45:46 -0300

    Update Romance.java
    não sei se tá certo fazer dessa forma, mas vê o que vc acha, vai ver ajuda a pensar de outras formas ne

--- bilboteca/Romance.java
+++ bilboteca/Romance.java
@@ -5,7 +5,7 @@ public class Romance extends Genero{
 	boolean fatosReais;
 	
 	 public Romance () {
-		Romance r1 = new Romance();
+		Romance r1 = new Romance(); 
 		r1.genero = "Romance";
 		r1.nome = "Assassinato no Expresso do Oriente";
 		
