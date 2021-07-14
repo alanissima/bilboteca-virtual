9669753b8dfe7967ac069f055e6b3b29ec80d2ff
nicollytavassi <55161442+nicollytavassi@users.noreply.github.com>
2021-07-14 04:37:31 -0300

    Update Romance.java

--- bilboteca/Romance.java
+++ bilboteca/Romance.java
@@ -1,11 +1,16 @@
 package bilboteca;
 
 public class Romance extends Genero{
-	
+
 	boolean fatosReais;
 	
-	public Romance() {
-		nome = "Romance";
-		id = 200;
+	 public Romance () {
+		Romance r1 = new Romance();
+		r1.genero = "Romance";
+		r1.nome = "Assassinato no Expresso do Oriente";
+		
+		id =  200 ;
+		
 	}
+	
 }
