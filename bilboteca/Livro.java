e67acfc62f0aa3606841c6358203bba9530a6577
nicollytavassi <55161442+nicollytavassi@users.noreply.github.com>
2021-07-14 04:36:42 -0300

    Update Livro.java

--- bilboteca/Livro.java
+++ bilboteca/Livro.java
@@ -1,15 +1,10 @@
 package bilboteca;
 
-public class Livro {
-	String titulo;
-	String autor;
-	int ano;
-	int id;
-	String editora;
-	Genero genero;
-	String sinopse;
+public class Livro extends Biblioteca{
+
 	
-	public Livro(String titulo,	String autor, int ano,	int id, String editora,	Genero genero, String sinopse) {
+	
+	 public Livro ( String  titulo ,String  autor , int  ano ,	 int  id , String  editora ,	 Genero  genero , String  sinopse ) {
 		this.titulo = titulo;
 		this.autor = autor;
 		this.ano = ano;
@@ -19,6 +14,17 @@ public class Livro {
 		this.sinopse = sinopse;		
 	}
 
-	public void crialivro() {}
-	public void expoeLivro() {}
+	public  void  crialivro () {
+	
+		
+	}
+	public  void  expoeLivro () {
+		System.out.println("Titulo do livro: " + titulo);
+		System.out.println("Autor: " + autor);
+		System.out.println("ano: " + ano);
+		System.out.println("Editora: " + editora);
+		System.out.println("Genero: " + genero);
+		System.out.println("Sinopse: " + sinopse);
+	}
+
 }
